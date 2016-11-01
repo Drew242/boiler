bcea6d193d44ca4ef16346dd2ca8b05d0543258b
Drew Conly <drew.conly@cherwell.com>
2016-11-01 11:37:13 -0600

    add some data binding

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,27 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <h1>{{ banner }}</h1>
+        <p class="hover" [class.clicked]="showDetail" (click)="onSelect()" >{{ name }}</p>
+        <input [(ngModel)]="twoWay" type="text">
+        <div *ngIf="showDetail">
+          <p>
+            Name: {{ contact.firstName }}
+            {{ contact.lastName }}<br>
+            Email: {{ contact.email }}
+          </p>
+        </div>
+        <p>{{ twoWay }}</p>
     `,
 })
 export class AppComponent {
+    public banner = 'Angular 2 Boilerplate';
+    public name = 'Hello World';
+    public contact = { firstName: 'Drew', lastName: 'Conly', email: 'atconly@gmail.com' }
+    public showDetail = false;
+    public twoWay = "";
 
+    onSelect() {
+      this.showDetail = !this.showDetail;
+    }
 }
